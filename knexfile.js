cfc7d282aca798ca8d7367f360233510e212c72c
badCompany55 <zachirvin45@gmail.com>
2019-12-09 19:46:53 -0600

    config

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-		connection: process.env.DB_URL,
+		connection: process.env.DATABASE_URL,
 		ssl: true,
 		pool: {
 			min: 2,
@@ -23,7 +23,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-		connection: process.env.DB_URL,
+		connection: process.env.DATABASE_URL,
 		ssl: true,
 		pool: {
 			min: 2,
