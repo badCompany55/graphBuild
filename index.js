8b8cfc739e91104cb20e5638043bad4f3499a9cb
badCompany55 <zachirvin45@gmail.com>
2019-12-09 19:31:01 -0600

    config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {ApolloServer, gql} = require('apollo-server')
 const resolvers = require('./resolvers.js')
 const typeDefs = require('./schema.js')
-const Port =  8000
+const Port = process.env.PORT || 8000
 const usersModel = require('./data/models/usersModel.js')
 const messagesModel = require('./data/models/messagesModel.js')
 const bcryptHashFunctions = require('./helpers/auth.js')
@@ -20,6 +20,6 @@ const server = new ApolloServer({typeDefs, resolvers, context: ({req}) => {
 	}
 }})
 
-server.listen(process.env.PORT || Port, () => {
+server.listen(Port, () => {
 	console.log(`Server running on Port: ${Port}`)
 })
